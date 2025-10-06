99b6e507728d81ca6e722122e6391bb888603fe9
kkkp4 <152593844+kkkp4@users.noreply.github.com>
2025-10-06 19:50:31 +0700

    Update main.cpp

--- motor/src/main.cpp
+++ motor/src/main.cpp
@@ -11,22 +11,62 @@ Motor motors[4] = {
   {4, 18, 0, 1},    // Motor 1
   {21, 22, 2, 3},   // Motor 2
   {19, 23, 4, 5},   // Motor 3 
-  {32, 33, 6, 7}    // Motor 4
+  {25, 26, 6, 7}    // Motor 4
 };
 
-int motorSpeed = 150; // 0-255
+#define ENCODER_L_A 34
+#define ENCODER_L_B 35
+#define ENCODER_R_A 32
+#define ENCODER_R_B 33
+
+volatile long ticks_left = 0;
+volatile long ticks_right = 0;
+
+const float wheel_diameter_mm = 47.0;
+const int gear_ratio = 90;
+const int encoder_cpr = 11;
+const int ticks_per_rev = encoder_cpr * gear_ratio;
+const float wheel_circumference_mm = 3.1416 * wheel_diameter_mm;
+const float mm_per_tick = wheel_circumference_mm / ticks_per_rev;
+
+int motorSpeed = 150;
+unsigned long lastReport = 0;
+
+// ************* ENCODER ISR (แก้ทิศทางแล้ว!) *************
+void IRAM_ATTR leftEncoderISR() {
+  if (digitalRead(ENCODER_L_A) == digitalRead(ENCODER_L_B))
+    ticks_left--;      // เดิม ++
+  else
+    ticks_left++;      // เดิม --
+}
+
+void IRAM_ATTR rightEncoderISR() {
+  if (digitalRead(ENCODER_R_A) == digitalRead(ENCODER_R_B))
+    ticks_right--;     // เดิม ++
+  else
+    ticks_right++;     // เดิม --
+}
+// *******************************************************
 
 void setup() {
   Serial.begin(115200);
 
   for (int i = 0; i < 4; i++) {
-    ledcSetup(motors[i].channel1, 20000, 8); // 20kHz, 8-bit
+    ledcSetup(motors[i].channel1, 20000, 8);
     ledcSetup(motors[i].channel2, 20000, 8);
     ledcAttachPin(motors[i].in1, motors[i].channel1);
     ledcAttachPin(motors[i].in2, motors[i].channel2);
   }
 
-  Serial.println("ESP32 + MDD3A Ready!");
+  pinMode(ENCODER_L_A, INPUT);
+  pinMode(ENCODER_L_B, INPUT);
+  pinMode(ENCODER_R_A, INPUT);
+  pinMode(ENCODER_R_B, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCODER_L_A), leftEncoderISR, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_R_A), rightEncoderISR, CHANGE);
+
+  Serial.println("ESP32 + Encoder Direction Fixed Ready!");
 }
 
 void setMotor(int id, char dir, int speed) {
@@ -38,7 +78,7 @@ void setMotor(int id, char dir, int speed) {
   } else if (dir == 'B') {
     ledcWrite(motors[id].channel1, 0);
     ledcWrite(motors[id].channel2, speed);
-  } else { // Stop
+  } else {
     ledcWrite(motors[id].channel1, 0);
     ledcWrite(motors[id].channel2, 0);
   }
@@ -65,13 +105,11 @@ void loop() {
     } else if (direction == 'B') {
       for (int i = 0; i < 4; i++) setMotor(i, 'B', motorSpeed);
     } else if (direction == 'L') {
-      // ซ้าย: มอเตอร์ซ้ายถอย, มอเตอร์ขวาเดินหน้า
       setMotor(0, 'B', motorSpeed);
       setMotor(1, 'B', motorSpeed);
       setMotor(2, 'F', motorSpeed);
       setMotor(3, 'F', motorSpeed);
     } else if (direction == 'R') {
-      // ขวา
       setMotor(0, 'F', motorSpeed);
       setMotor(1, 'F', motorSpeed);
       setMotor(2, 'B', motorSpeed);
@@ -81,5 +119,32 @@ void loop() {
     }
 
     Serial.println("OK: " + cmd);
+  }
+
+  if (millis() - lastReport >= 500) {
+    static long lastL = 0;
+    static long lastR = 0;
+
+    long nowL = ticks_left;
+    long nowR = ticks_right;
+    long deltaL = nowL - lastL;
+    long deltaR = nowR - lastR;
+
+    float vL = (deltaL * mm_per_tick) / 0.5;
+    float vR = (deltaR * mm_per_tick) / 0.5;
+
+    float dL = nowL * mm_per_tick;
+    float dR = nowR * mm_per_tick;
+
+    Serial.print("L_TICKS:"); Serial.print(nowL);
+    Serial.print(",R_TICKS:"); Serial.print(nowR);
+    Serial.print(",L_VEL:"); Serial.print(vL, 2);
+    Serial.print(",R_VEL:"); Serial.print(vR, 2);
+    Serial.print(",L_DIST:"); Serial.print(dL, 2);
+    Serial.print(",R_DIST:"); Serial.println(dR, 2);
+
+    lastL = nowL;
+    lastR = nowR;
+    lastReport = millis();
   }
 }
