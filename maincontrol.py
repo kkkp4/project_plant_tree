3a6ba7acdfc366e826bc3ae3a36b873ed512bd3e
kkkp4 <152593844+kkkp4@users.noreply.github.com>
2025-10-06 20:14:43 +0700

    Update maincontrol.py

--- maincontrol.py
+++ maincontrol.py
@@ -39,7 +39,7 @@ class TeleopSerial(Node):
             actions = {
                 'w': 'F', 's': 'B', 'a': 'L', 'd': 'R', ' ': 'STOP_ALL',
                 'j': 'STEP1_FWD', 'k': 'STEP1_BWD', 'l': 'STEP2_45',
-                'o': 'SERVO1_TOGGLE', 'u': 'SERVO2_TOGGLE',
+                'o': 'SHAKE', 'u': 'SERVO2_TOGGLE',
                 'z': ('SERVO1', 70), 'x': ('SERVO1', 150),
                 'c': ('SERVO2', 0), 'v': ('SERVO2', 90),
                 'i': 'MOTOR1_TOGGLE',
